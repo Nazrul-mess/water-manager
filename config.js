85f341916dcb0ca818ab0ffe82c4c533985f89f1
Nazrul-mess <messnazrul@gmail.com>
2026-07-21 15:23:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,30 +1,15 @@
 
-// export const firebaseConfig = {
-//     apiKey: "AIzaSyBXQfK7GsFhnoqQ-CCBALUpAt_mqrW3ynU",
-//     authDomain: "waterdb-58644.firebaseapp.com",
-//     projectId: "waterdb-58644",
-//     storageBucket: "waterdb-58644.firebasestorage.app",
-//     messagingSenderId: "189956996443",
-//     appId: "1:189956996443:web:5b1d568f663283957cae7e"
-// };
-
 export const firebaseConfig = {
-    apiKey: "AIzaSyAyLGGhDUQSVPFNrdqrNgcrllw3F1jKkDw",
-    authDomain: "sahin-bedb5.firebaseapp.com",
-    databaseURL: "https://sahin-bedb5-default-rtdb.firebaseio.com",
-    projectId: "sahin-bedb5",
-    storageBucket: "sahin-bedb5.firebasestorage.app",
-    messagingSenderId: "284579138978",
-    appId: "1:284579138978:web:4f63446361ebef3b3076fb",
-    measurementId: "G-8GSTXKCP6M"
-  };
+    apiKey: "AIzaSyBXQfK7GsFhnoqQ-CCBALUpAt_mqrW3ynU",
+    authDomain: "waterdb-58644.firebaseapp.com",
+    projectId: "waterdb-58644",
+    storageBucket: "waterdb-58644.firebasestorage.app",
+    messagingSenderId: "189956996443",
+    appId: "1:189956996443:web:5b1d568f663283957cae7e"
+};
 
 
-
-// export const GOOGLE_SHEET_WEBHOOK = "https://script.google.com/macros/s/AKfycbwf3pSmHcM-gPMkgmdoFMYe_0Qwa0F2MQg_eZuBKBjJMgjSwo1sUGLPtqVu4NsPMWzbrg/exec";
-
-export const GOOGLE_SHEET_WEBHOOK = "https://script.google.com/macros/s/gggd/exec";
-
+export const GOOGLE_SHEET_WEBHOOK = "https://script.google.com/macros/s/AKfycbwf3pSmHcM-gPMkgmdoFMYe_0Qwa0F2MQg_eZuBKBjJMgjSwo1sUGLPtqVu4NsPMWzbrg/exec";
 
 
 export const DEFAULT_ROOM_PASSWORD = "room123";
